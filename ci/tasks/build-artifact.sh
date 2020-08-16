bc15d53ce91c51e1069bfffbf1e9ca80ecead06b
dektlong <dekel.tankel@gmail.com>
2020-08-15 23:37:44 -0700

    Update build-artifact.sh

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,8 +5,6 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd git-repo
 gradle assemble -Pversion=$version
-cp build/libs/*.jar ../artifact-dir/rockme.1.0.0-rc.89.jar
-cd ../artifact-dir
-echo "files copied to artifact-dir"
-ls
+cp build/libs/*.jar ../artifact-dir/rockme-1.0.jar
+
 
